747a3ca032b88b41b308cb4cf3351535935d55c5
Rich <rpavesi84@gmail.com>
2020-04-29 14:09:51 -0400

    part 4 "init"

--- client/packs/web-store-bundle.js
+++ client/packs/web-store-bundle.js
@@ -3,9 +3,11 @@ import ReactOnRails from 'react-on-rails';
 import { SiteHeader } from '../bundles/WebStore/components/SiteHeader.jsx';
 import { ProductRow } from '../bundles/WebStore/components/ProductRow.jsx';
 import { ShoppingCartList } from '../bundles/WebStore/components/ShoppingCartList.jsx';
+import { CheckoutForm } from '../bundles/WebStore/components/CheckoutForm.jsx';
 
 ReactOnRails.register({
   SiteHeader,
   ProductRow,
-  ShoppingCartList
+  ShoppingCartList,
+  CheckoutForm
 });
